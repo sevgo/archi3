6534ae03c1374fe612eab0bf9159437a156eca04
Sevgin Mustafov <sevgin@nigves.com>
2017-06-25 19:02:16 +0300

    latest, working version of my configs

--- scripts/dualscreen_autoset.sh
+++ scripts/dualscreen_autoset.sh
@@ -2,8 +2,28 @@
 #
 # Dual monitor support
 
-if xrandr | grep -qe '^DP1 connected' 
+if xrandr | grep -q 'DVI-I-0 connected' && xrandr | grep -q 'DVI-I-3 connected'
 then
-	xrandr --output DP1 --mode 1920x1080 --right-of eDP1
+	xrandr --output DVI-I-0 --auto --left-of DVI-I-3
+
+fi
+
+if xrandr | grep -q 'DVI-0 connected' && xrandr | grep -q 'DVI-1 connected'
+then
+	xrandr --output DVI-0 --auto --left-of DVI-1
+
+fi
+
+
+if xrandr | grep -q 'DVI-I-1 connected' && xrandr | grep -q 'DVI-I-2 connected'
+then
+	xrandr --output DVI-I-1 --auto --left-of DVI-I-2
+
+fi
+
+
+if xrandr | grep -q 'DVI-I-2 connected' && xrandr | grep -q 'DVI-I-3 connected'
+then
+	xrandr --output DVI-I-2 --auto --left-of DVI-I-3
 
 fi
